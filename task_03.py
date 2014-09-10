d5321159b1b5ec3000d033b6e7e9a3da29a0bf49
Nikita Joseph <nikita.joseph@spsmail.cuny.edu>
2014-09-10 15:09:36 -0400

    Fixing task 3 please

--- task_03.py
+++ task_03.py
@@ -14,10 +14,10 @@ if CHOICE_1 == "Seattle Gray" or CHOICE_1 == "Manatee":
         if CHOICE_2 == "Ceramic Glaze" or "Cumulus Nimbus":
             ACCENT = CHOICE_2
             if ACCENT == "Ceramic Glaze":
-                HIGHLIGHTER= raw_input("Please choose between"
+                HIGHLIGHT= raw_input("Please choose between"
                                        " Basically White or White")
             elif ACCENT == "Cumulus Nimbus":
-                HIGHLIGHTER = raw_input("Please choose between"
+                HIGHLIGHT = raw_input("Please choose between"
                                         " Off-White or Paper White")
     elif BASE == "Manatee":
         CHOICE_2 = raw_input("Please choose between Platinum Mist"
@@ -25,13 +25,13 @@ if CHOICE_1 == "Seattle Gray" or CHOICE_1 == "Manatee":
         if CHOICE_2 == "Platinum Mist" or "Spartan Sage":
             ACCENT = CHOICE_2
             if ACCENT == "Platinum Mist":
-                HIGHLIGHTER = raw_input("Please choose between"
+                HIGHLIGHT = raw_input("Please choose between"
                                         " Bone White or Just White")
             elif ACCENT == "Spartan Sage":
-                HIGHLIGHTER = raw_input("Please choose between"
+                HIGHLIGHT = raw_input("Please choose between"
                                         " Fractal White or Not White")
 
 
 print "The colors are {c1}, {c2}, {c3}.".format(c1=BASE, c2= ACCENT,
-                                                c3=HIGHLIGHTER)
+                                                c3=HIGHLIGHT)
         
