ad2a7f2639249e2d9114c2849a47162a195c9d80
Nikita Joseph <nikita.joseph@spsmail.cuny.edu>
2014-09-10 13:33:41 -0400

    Task 3

--- task_03.py
+++ task_03.py
@@ -7,12 +7,12 @@ CHOICE_1 = raw_input("Please choose between Seattle Gray"
 CHOICE_2 = raw_input("Please choose between Ceramic Glaze"
                      " or Cumulus Nimbus ")
 CHOICE_7 = raw_input("Please choose between Basically White"
-                     " or White ")
+                     " or Off-White ")
 if CHOICE_1 == "Seattle Gray" or CHOICE_1 == "Manatee":
     BASE = CHOICE_1
     if CHOICE_2 == "Ceramic Glaze" or CHOICE_2 == "Cumulus Nimbus":
         ACCENT = CHOICE_2
-    if CHOICE_7 == "Basically White" or CHOICE_7 == "White":
+    if CHOICE_7 == "Basically White" or CHOICE_7 == "Off-White":
             HIGHLIGHT = CHOICE_7
 
 print "The colors chosen are {a}, {b}, {c}.".format(a=BASE, b=ACCENT,
