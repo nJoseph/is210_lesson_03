73f21fba9987416b250688d86c6de5d84999d721
Nikita Joseph <nikita.joseph@spsmail.cuny.edu>
2014-09-10 15:04:09 -0400

    fixed task 3 hopefully...

--- task_03.py
+++ task_03.py
@@ -4,16 +4,33 @@
 
 CHOICE_1 = raw_input("Please choose between Seattle Gray"
                      " or Manatee ")
-CHOICE_2 = raw_input("Please choose between Ceramic Glaze"
-                     " or Cumulus Nimbus ")
-CHOICE_7 = raw_input("Please choose between Basically White"
-                     " or Off-White ")
+CHOICE_2 = ""
+CHOICE_3 = ""
 if CHOICE_1 == "Seattle Gray" or CHOICE_1 == "Manatee":
     BASE = CHOICE_1
-    if CHOICE_2 == "Ceramic Glaze" or CHOICE_2 == "Cumulus Nimbus":
-        ACCENT = CHOICE_2
-    if CHOICE_7 == "Basically White" or CHOICE_7 == "Off-White":
-            HIGHLIGHT = CHOICE_7
+    if BASE == "Seattle Gray":
+        CHOICE_2 = raw_input("Please choose between"
+                              " Ceramic Glaze or Cumulus Nimbus")
+        if CHOICE_2 == "Ceramic Glaze" or "Cumulus Nimbus":
+            ACCENT = CHOICE_2
+            if ACCENT == "Ceramic Glaze":
+                HIGHLIGHTER= raw_input("Please choose between"
+                                       " Basically White or White")
+            elif ACCENT == "Cumulus Nimbus":
+                HIGHLIGHTER = raw_input("Please choose between"
+                                        " Off-White or Paper White")
+    elif BASE == "Manatee":
+        CHOICE_2 = raw_input("Please choose between Platinum Mist"
+                             " or Spartan Sage")
+        if CHOICE_2 == "Platinum Mist" or "Spartan Sage":
+            ACCENT = CHOICE_2
+            if ACCENT == "Platinum Mist":
+                HIGHLIGHTER = raw_input("Please choose between"
+                                        " Bone White or Just White")
+            elif ACCENT == "Spartan Sage":
+                HIGHLIGHTER = raw_input("Please choose between"
+                                        " Fractal White or Not White")
 
-print "The colors chosen are {a}, {b}, {c}.".format(a=BASE, b=ACCENT,
-                                                    c=HIGHLIGHT)
+
+print BASE, ACCENT, HIGHLIGHTER
+        
